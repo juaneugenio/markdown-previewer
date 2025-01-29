040c6e5da53d37bc5421c6e471cf7dac250566c3
juaneugenio <leuviah77@gmail.com>
2025-01-29 15:46:27 +0100

    Implemented tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    <h1>App.jsx</h1>
-      
-    </>
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center text-gray-800">
+          Markdown Previewer
+        </h1>
+      </div>
+    </div>
   )
 }
 
